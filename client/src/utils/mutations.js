b8e104c68fd586045211ab970b560a922c8cfd12
Mikekuney <96913841+Mikekuney@users.noreply.github.com>
2022-07-03 18:04:35 -0400

    updated quries.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,11 +25,11 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook($bookData: BookInput!) {
-    saveBook(bookData: $bookData) {
-      _id
+  mutation saveBook($book: SaveBookInput!) {
+    saveBook(book: $book) {
       username
       email
+      bookCount
       savedBooks {
         bookId
         authors
@@ -43,11 +43,11 @@ export const SAVE_BOOK = gql`
 `;
 
 export const REMOVE_BOOK = gql`
-  mutation removeBook($bookId: ID!) {
+  mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
-      _id
       username
       email
+      bookCount
       savedBooks {
         bookId
         authors
