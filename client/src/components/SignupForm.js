be8f914cf5a96e5433f7b7d655acd1d2661697ca
Mikekuney <96913841+Mikekuney@users.noreply.github.com>
2022-07-03 18:41:16 -0400

    tweaked Signupform.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -8,7 +8,8 @@ import { ADD_USER } from '../utils/mutations';
 const SignupForm = () => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
-  const [addUser, { error }] = useMutation(ADD_USER);
+  // define mutation for adding new user
+  const [addUser] = useMutation(ADD_USER);
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
@@ -35,8 +36,9 @@ const SignupForm = () => {
       });
 
       Auth.login(data.addUser.token);
-    } catch (e) {
-      console.error(e);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
     }
 
     setUserFormData({
